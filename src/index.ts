470d54877bc6d688a6441aa491409868ca0775a6
Jens Pots <jens@jenspots.com>
2024-02-13 12:40:35 +0100

    feat: dummy implementation of process

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import { Writer } from "@treecg/connector-types";
+
+export async function httpFetch(
+    message: string,
+    writer: Writer<string | Buffer>,
+    closeOnEnd: boolean = true,
+) {
+    // This is a source processor (i.e, the first processor in a pipeline),
+    // therefore we should wait until the rest of the pipeline is set
+    // to start pushing down data
+    return async () => {
+        await writer.push(message);
+
+        if (closeOnEnd) {
+            await writer.end();
+        }
+    };
+}
