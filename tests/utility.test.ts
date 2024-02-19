a8ceb16960150b29547280fb409f2e66713877d8
Jens Pots <jens@jenspots.com>
2024-02-19 22:28:36 +0100

    test: utilities

---
+++ tests/utility.test.ts
@@ -0,0 +1,62 @@
+import { describe, expect, test } from "bun:test";
+import { parseHeaders, statusCodeAccepted } from "../src/util";
+import { HttpUtilsError } from "../src/error";
+
+describe("parseHeaders", () => {
+    test("empty", () => {
+        const headers: string[] = [];
+        const obj = parseHeaders(headers);
+        expect(obj.count).toBe(0);
+    });
+
+    test("valid", () => {
+        const headers = ["a: b", "c: d"];
+        const obj = parseHeaders(headers);
+        expect(obj.get("a")).toBe("b");
+        expect(obj.get("c")).toBe("d");
+        expect(obj.count).toBe(2);
+    });
+
+    test("invalid", () => {
+        const headers = ["a: b", "c"];
+        expect(() => parseHeaders(headers)).toThrow(
+            HttpUtilsError.invalidHeaders(),
+        );
+    });
+});
+
+describe("statusCodeAccepted", () => {
+    const validInput = "100, 200-300, 400";
+
+    test("contained as literal", () => {
+        expect(statusCodeAccepted(100, validInput)).toBe(true);
+    });
+
+    test("contained as range", () => {
+        expect(statusCodeAccepted(299, validInput)).toBe(true);
+    });
+
+    test("not contained", () => {
+        expect(statusCodeAccepted(150, validInput)).toBe(false);
+    });
+
+    test("not contained as end of range", () => {
+        expect(statusCodeAccepted(300, validInput)).toBe(false);
+    });
+
+    test("no accepted status codes", () => {
+        expect(statusCodeAccepted(200, "")).toBe(false);
+    });
+
+    test("invalid range - delimiter", () => {
+        expect(() => statusCodeAccepted(0, "200_300")).toThrow(
+            HttpUtilsError.invalidStatusCodeRange(),
+        );
+    });
+
+    test("invalid range - non numerical", () => {
+        expect(() => statusCodeAccepted(0, "a-b")).toThrow(
+            HttpUtilsError.invalidStatusCodeRange(),
+        );
+    });
+});
