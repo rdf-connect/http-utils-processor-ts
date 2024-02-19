d7b3df3294aee4a64bafe28eafbc0a601e5c6f87
Jens Pots <jens@jenspots.com>
2024-02-19 13:02:12 +0100

    test: additional status code checks

--- tests/functional.test.ts
+++ tests/functional.test.ts
@@ -1,18 +1,29 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test } from "bun:test";
 import { SimpleStream } from "@ajuvercr/js-runner";
 import { httpFetch } from "../src";
+import {HttpUtilsError} from "../src/error";
 
 describe("Functional tests for the httpFetch Connector Architecture function", () => {
     // We initialize a simple Bun server to test our process against. Note that
-    // since we cannot simply use `expect` inside of the `fetch` function, so we 
+    // since we cannot simply use `expect` inside the `fetch` function, so we
     // pass the required values to the client using the response body.
     const server = Bun.serve({
         fetch(req) {
+            const requestUrl = new URL(req.url);
+            const statusQuery = requestUrl.searchParams.get("status");
+            const status = statusQuery ? Number(statusQuery) : 200;
+
+            if (Number.isNaN(status)) {
+                throw Error("Invalid status code");
+            }
+
             return new Response(JSON.stringify({
                 greeting: "Hello, World!",
                 headers: req.headers,
                 method: req.method,
-            }));
+            }), {
+                status,
+            });
         },
     });
 
@@ -44,5 +55,68 @@ describe("Functional tests for the httpFetch Connector Architecture function", (
             true,
             ["Content-Type: text/plain", "Accept: text/plain"],
         ))();
+    });
+
+    test("Invalid status throws error (default).", async () => {
+        const writeStream = new SimpleStream<Buffer>();
+
+        // Await and execute returned function of processor.
+        const func = await httpFetch(
+            `${server.url.toString()}?status=500`,
+            "GET",
+            writeStream,
+            true,
+            ["Content-Type: text/plain", "Accept: text/plain"],
+        );
+
+        expect(func()).rejects.toThrow(HttpUtilsError.statusCodeNotAccepted(500));
+    });
+
+    test("Deny 200 status code", async () => {
+        const writeStream = new SimpleStream<Buffer>();
+
+        // Await and execute returned function of processor.
+        const func = await httpFetch(
+            `${server.url.toString()}?status=200`,
+            "GET",
+            writeStream,
+            true,
+            ["Content-Type: text/plain", "Accept: text/plain"],
+            "201-300",
+        );
+
+        expect(func()).rejects.toThrow(HttpUtilsError.statusCodeNotAccepted(200));
+    })
+
+    test("Explicitly accepted status - range", async () => {
+        const writeStream = new SimpleStream<Buffer>();
+
+        // Await and execute returned function of processor.
+        const func = await httpFetch(
+            `${server.url.toString()}?status=501`,
+            "GET",
+            writeStream,
+            true,
+            ["Content-Type: text/plain", "Accept: text/plain"],
+            "100,500-502,505",
+        );
+
+        await func();
+    });
+
+    test("Explicitly accepted status - single", async () => {
+        const writeStream = new SimpleStream<Buffer>();
+
+        // Await and execute returned function of processor.
+        const func = await httpFetch(
+            `${server.url.toString()}?status=500`,
+            "GET",
+            writeStream,
+            true,
+            ["Content-Type: text/plain", "Accept: text/plain"],
+            "200-300,500,503",
+        );
+
+        await func();
     });
 });
